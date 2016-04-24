e80027d62bade5293664d108193cb39f77a98766
colsill <colmsills@yahoo.com>
2016-04-25 00:55:45 +0100

    add calculator class methods

--- DevOp/src/main/java/com/DevOp/Calculator.java
+++ DevOp/src/main/java/com/DevOp/Calculator.java
@@ -2,21 +2,22 @@ package com.DevOp;
 
 
 public class Calculator {
-
-	public int sum(int a, int b) {
-		return a + b;
-	}
-
-	public int minus(int a, int b) {
-		return a + b;  // ERROR!!!
-	}
-
-	public int divide(int a, int b) {
-		return a / b;
-	}
-
-	public int multiply(int a, int b) {
-		return a * b;
-	}
-
+	 public double add(double a, double b) {
+		  return a + b;
+		 }
+		  
+		 public double subtract(double a, double b) {
+		  return a - b;
+		 }
+		  
+		 public double multiply(double a, double b) {
+		  return a * b;
+		 }
+		  
+		 public double divide(double a, double b) { 
+		  if(b == 0) {
+		   throw new ArithmeticException();
+		  }
+		  return a / b;
+		 }
 }
