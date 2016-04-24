b15d85905b0fa4e18983b3903c652a82e5950614
colsill <colmsills@yahoo.com>
2016-04-25 00:33:58 +0100

    Calculator class

--- DevOp/src/main/java/com/DevOp/Calculator.java
+++ DevOp/src/main/java/com/DevOp/Calculator.java
@@ -1,5 +1,22 @@
 package com.DevOp;
 
+
 public class Calculator {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int minus(int a, int b) {
+		return a + b;  // ERROR!!!
+	}
+
+	public int divide(int a, int b) {
+		return a / b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
 
 }
