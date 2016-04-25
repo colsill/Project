502b7eda07f3c3b57246fb40f303e3fafb2b1b59
colsill <colmsills@yahoo.com>
2016-04-25 01:19:30 +0100

    Test cases JUnit

--- DevOp/src/test/java/com/DevOp/CalculatorTest.java
+++ DevOp/src/test/java/com/DevOp/CalculatorTest.java
@@ -1,23 +1,53 @@
 package com.DevOp;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import junit.framework.TestCase;
+
 
 public class CalculatorTest extends TestCase {
 
-	public void testAdd() {
-		fail("Not yet implemented");
-	}
-
-	public void testSubtract() {
-		fail("Not yet implemented");
-	}
-
-	public void testMultiply() {
-		fail("Not yet implemented");
-	}
-
-	public void testDivide() {
-		fail("Not yet implemented");
-	}
-
+	private Calculator calc;
+	  
+	@Before
+	 public void setUp() throws Exception {
+	  calc = new Calculator();
+	 }
+	 
+	 @After
+	 public void tearDown() throws Exception {
+	  calc = null;
+	 }
+	 
+	 @Test
+	 public void testAdd() {
+	  double result = calc.add(10.5, 20.32);
+	  assertEquals(30.82, result, 0);
+	 }
+	 
+	 @Test
+	 public void testSubtract() {
+	  double result = calc.subtract(100.5, 100);
+	  assertEquals(0.5, result, 0);
+	 }
+	 
+	 @Test
+	 public void testMultiply() {
+	  double result = calc.multiply(2.5, 100);
+	  assertEquals(250, result, 0);
+	 }
+	 
+	 @Test
+	 public void testDivide() {
+	  double result = calc.divide(100, 10);
+	  assertEquals(10, result, 0.001);
+	   
+	 }
+	  
+	 @Test(expected = ArithmeticException.class)
+	 public void testDivideByZero() {
+	  calc.divide(100.5, 0);
+	 }
 }
