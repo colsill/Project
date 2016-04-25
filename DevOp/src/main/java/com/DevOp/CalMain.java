54356afde809f46e76e72a88db42be862739f7b1
colsill <colmsills@yahoo.com>
2016-04-25 10:31:03 +0100

    adding main

---
+++ DevOp/src/main/java/com/DevOp/CalMain.java
@@ -0,0 +1,21 @@
+package com.DevOp;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class CalMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator cal=new Calculator();
+		JFrame frame = new JFrame("Answer");
+		
+		double n1=Integer.parseInt(JOptionPane.showInputDialog("Please enter 1st number to add?"));
+		double n2=Integer.parseInt(JOptionPane.showInputDialog("Please enter 2st number to add?"));
+		double answerAdd=cal.add(n1,n2);
+		JOptionPane.showMessageDialog(frame, answerAdd);
+		
+
+	}
+
+}
