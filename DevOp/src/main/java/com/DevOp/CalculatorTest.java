da8f104a3dae6d1476d4b124fee83aba51ba72e9
colsill <colmsills@yahoo.com>
2016-04-26 14:12:02 +0100

    test case ammend

--- DevOp/src/main/java/com/DevOp/CalculatorTest.java
+++ DevOp/src/main/java/com/DevOp/CalculatorTest.java
@@ -3,6 +3,17 @@ package com.DevOp;
 import junit.framework.TestCase;
 
 public class CalculatorTest extends TestCase {
+	
+	/* @Before
+	 public void setUp() throws Exception {
+	  calc = new Calculator();
+	 }
+
+	public void testAdd() {
+		 double result = calc.add(10, 20);
+		  assertEquals(30, result, 0);
+	}
+	*/
 
 	public void testAdd() {
 		fail("Not yet implemented");
